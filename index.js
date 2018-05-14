ba2043f1abfa427c1a803a60279329d7ec7c9fc4
Duane Felton <dfelton2010@gmail.com>
2018-05-14 16:51:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 const facts = [
-  "He was the last Beatle to "]
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var lennonFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    lennonFacts.push
+  }
+}
