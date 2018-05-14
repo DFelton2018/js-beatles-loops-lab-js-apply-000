a7ceedeb817b176b9cfb7702bd4e20a030be8af1
Duane Felton <dfelton2010@gmail.com>
2018-05-14 16:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function johnLennonFacts(facts) {
   var lennonFacts = [];
   var i = 0;
   while (i < facts.length) {
-    lennonFacts.push
+  lennonFacts.push(facts[i] + "!!!");
+  i++;  
   }
 }
